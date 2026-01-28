9d2b0996c95bc247b0e14680309fe46c28399fb9
ctrl-404 <ctrl-404@email.com>
2026-01-28 12:24:48 +0000

    added first simple junit tests #2

--- src/test/java/de/htwsaar/Berta/AppTest.java
+++ src/test/java/de/htwsaar/Berta/AppTest.java
@@ -1,20 +1,13 @@
 package de.htwsaar.Berta;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
 
-import org.junit.Test;
-
-/**
- * Unit test for simple App.
- */
 public class AppTest 
 {
-    /**
-     * Rigorous Test :-)
-     */
-    @Test
-    public void shouldAnswerWithTrue()
-    {
-        assertTrue( true );
-    }
+  @Test
+  public void shouldAnswerWithTrue()
+  {
+    assertTrue( true );
+  }
 }
